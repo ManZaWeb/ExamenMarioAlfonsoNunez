5b83138263cf1e15eb8392073e0d6285de4c9f4e
Mario <maalnu93@hotmail.com>
2023-10-11 09:16:08 +0200

    v.1.1 JS ya funciona

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,18 @@
 document.addEventListener("DOMContentLoaded", function (){
 
-const ancho = parseFloat(document.getElementById("ancho").value)
-const long = parseFloat(document.getElementById("long").value)
 const calcu = document.getElementById("calcu")
 const m1 = document.getElementById("m1")
 const m2 = document.getElementById("m2")
 
-calcular.addEventListener('click', function (){
+calcu.addEventListener('click', function (){
+    const ancho = parseFloat(document.getElementById("ancho").value)
+    const long = parseFloat(document.getElementById("long").value)
     let area = long * ancho
     let perimetro = (long + ancho) * 2
     let diagonal = Math.sqrt(Math.pow(long, 2) + Math.pow(ancho, 2))
+
+    m1.innerHTML = "Área del rectangulo: " + area
+    m2.innerHTML = "Perímetro del rectangulo: " + perimetro
+    m3.innerHTML = "Diagonal del rectangulo: " + diagonal
 } )
 })
